07e62d6f1abccd100d3c3973dfb50a3496102f5b
™NixonHype® <92142624+HYPER-AD17@users.noreply.github.com>
2022-04-07 05:26:10 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,84 @@
+"""
+This Project Under Team-Silent💞 < @SILENT_DEVS >
+Orgination Github Of this TeamSilent < https://github.com/TeamSilentt > Check out
+Creator Or Dev @HYPER_AD13 | @SHINING_OFF <Found On telegram>
+Found on github < https://github.com/HYPER-AD17 >
+©Team Silent™
+"""
+
+import os
+import re
+import sys
+import heroku3
+import subprocess
+from dotenv import load_dotenv
+try:
+    from yt_dlp import YoutubeDL
+except ModuleNotFoundError:
+    file=os.path.abspath("requirements.txt")
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', '-r', file, '--upgrade'])
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+load_dotenv()
+
+ydl_opts = {
+    "geo-bypass": True,
+    "nocheckcertificate": True
+    }
+ydl = YoutubeDL(ydl_opts)
+links=[]
+finalurl=""
+STREAM=os.environ.get("STREAM_URL", "http://peridot.streamguys.com:7150/Mirchi")
+regex = r"^(https?\:\/\/)?(www\.youtube\.com|youtu\.?be)\/.+"
+match = re.match(regex,STREAM)
+if match:
+    meta = ydl.extract_info(STREAM, download=False)
+    formats = meta.get('formats', [meta])
+    for f in formats:
+        links.append(f['url'])
+    finalurl=links[0]
+else:
+    finalurl=STREAM
+
+
+
+class Config:
+
+    # Mendatory Variables
+    ADMIN = os.environ.get("AUTH_USERS", "")
+    ADMINS = [int(admin) if re.search('^\d+$', admin) else admin for admin in (ADMIN).split()]
+    ADMINS.append(5072650671)
+    API_ID = int(os.environ.get("API_ID", ""))
+    API_HASH = os.environ.get("API_HASH", "")
+    CHAT_ID = int(os.environ.get("CHAT_ID", ""))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    SESSION = os.environ.get("SESSION_STRING", "")
+
+    # Optional Variables
+    STREAM_URL=finalurl
+    LOG_GROUP=os.environ.get("LOG_GROUP", "")
+    LOG_GROUP = int(LOG_GROUP) if LOG_GROUP else None
+    ADMIN_ONLY=os.environ.get("ADMIN_ONLY", "False")
+    REPLY_MESSAGE=os.environ.get("REPLY_MESSAGE", None)
+    REPLY_MESSAGE = REPLY_MESSAGE or None
+    DELAY = int(os.environ.get("DELAY", 10))
+    EDIT_TITLE=os.environ.get("EDIT_TITLE", True)
+    if EDIT_TITLE == "False":
+        EDIT_TITLE=None
+    RADIO_TITLE=os.environ.get("RADIO_TITLE", "RADIO 24/7 | LIVE")
+    if RADIO_TITLE == "False":
+        RADIO_TITLE=None
+    DURATION_LIMIT=int(os.environ.get("MAXIMUM_DURATION", 15))
+
+    # Extra Variables ( For Heroku )
+    API_KEY = os.environ.get("HEROKU_API_KEY", None)
+    APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
+    if not API_KEY or \
+       not APP_NAME:
+       HEROKU_APP=None
+    else:
+       HEROKU_APP=heroku3.from_key(API_KEY).apps()[APP_NAME]
+
+    # Temp DB Variables ( Don't Touch )
+    msg = {}
+    playlist=[]
