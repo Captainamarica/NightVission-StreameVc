157ee695de85a718f7ebf71bc670623f259c8c51
™NixonHype® <92142624+HYPER-AD17@users.noreply.github.com>
2022-04-07 05:33:08 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+"""
+This Project Under Team-Silent💞 < @SILENT_DEVS >
+Orgination Github Of this TeamSilent < https://github.com/TeamSilentt > Check out
+Creator Or Dev @HYPER_AD13 | @SHINING_OFF <Found On telegram>
+Found on github < https://github.com/HYPER-AD17 >
+©Team Silent™
+"""
+
+echo "Cloning The repo, Please Wait..."
+git clone https://github.com/TeamSilentt/SilentRadioPlayer /SilentRadioPlayer
+echo "Installing All Requirements..."
+cd /SilentRadioPlayer
+pip3 install -U -r requirements.txt
+echo "Ok Starting Bot, Please Wait..."
+python3 main.py
